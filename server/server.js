1c2ddaf22127c454a3159692870767a87cbfba0e
Miguel Julio <mjulio@artnet.com>
2017-08-29 15:14:29 -0400

    add schema, and setup express to use graphql, graphiql

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
+import {
+    graphqlExpress,
+    graphiqlExpress,
+} from 'graphql-server-express';
+import bodyParser from 'body-parser';
 import express from 'express';
+
+import { schema } from './src/schema';
 
 const PORT = 4000;
 const server = express();
 
-server.get('/', (req, res) => {
-    res.send('hello world!')
-})
+server.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema
+}));
+
+server.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
+}));
 
 server.listen(PORT, () => console.log(`GraphQL Server is now running on http://localhost:${PORT}`));
---
+++ server/src/schema.js
@@ -0,0 +1,43 @@
+import {
+    makeExecutableSchema,
+    addMockFunctionsToSchema
+} from 'graphql-tools';
+
+const typeDefs = `
+    type User {
+        id: Int
+        name: String
+        posts(limit: Int): [Post]
+    }
+
+    type Post {
+        id: Int
+        title: String
+        views: Int
+        author: User
+    }
+
+    type Channel {
+        id: ID!
+        name: String!
+        topic: String!
+        userCount: Int
+        users: [User]
+
+    }
+
+    type channels {
+        channels: [Channel]
+    }
+
+    type Query {
+        author(id: Int): User
+        topPosts(limit: Int): [Post]
+        channels(limit: Int): [Channel]
+        channel: Channel
+    }
+`;
+
+const schema = makeExecutableSchema({ typeDefs });
+addMockFunctionsToSchema({ schema });
+export { schema };
