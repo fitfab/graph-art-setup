eed5f8cab76ca123ebd713139e0c7e539b23f254
Miguel Julio <mjulio@artnet.com>
2017-08-29 14:37:27 -0400

    create express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-console.log('this is running!')
+import express from 'express';
+
+const PORT = 4000;
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('hello world!')
+})
+
+server.listen(PORT, () => console.log(`GraphQL Server is now running on http://localhost:${PORT}`));
