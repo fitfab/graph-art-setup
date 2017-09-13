7b08e99bfc7c530db204284c5955188a75c05853
Miguel Julio <mjulio@artnet.com>
2017-09-13 10:42:12 -0400

    test 7

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,10 @@ class App extends Component {
                 <div className="App">
                     <div className="App-header">
                         <img src={logo} className="App-logo" alt="logo" />
-                        <h2 className="foo" data-ga="header">
+                        <h2
+                            className="foo"
+                            data-ga="header"
+                            >
                             Falcon | Graphql & React!
                         </h2>
                     </div>
